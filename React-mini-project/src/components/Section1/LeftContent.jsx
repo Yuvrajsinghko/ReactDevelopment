f0eba86081ffccff349e5a590ccce3dfa1212294
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-12-09 16:06:21 +0530

    modified some files in react mini project

--- React-mini-project/src/components/Section1/LeftContent.jsx
+++ React-mini-project/src/components/Section1/LeftContent.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-
+import "remixicon/fonts/remixicon.css";
 const LeftContent = () => {
   return (
-    <div className='h-full w-1/3 bg-amber-800'>
-        
-    </div>
-  )
-}
+    <div className="h-full p-4 flex flex-col justify-between w-1/3">
+      <div>
+        <h3 className="text-5xl mb-15 font-bold  ">
+          Prospective <br />
+          Customer <br />
+          segmentation
+        </h3>
 
-export default LeftContent
+        <p className="text-2xl font-medium text-gray-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+          hic nesciunt deserunt perspiciatis explicabo, quidem sit in quia
+          consequuntur adipisci ipsum rem voluptatibus possimus doloribus minima
+          quod odit nisi voluptate.
+        </p>
+      </div>
+      <div className="text-7xl">
+        <i class="ri-arrow-right-up-line"></i>
+      </div>
+    </div>
+  );
+};
+
+export default LeftContent;
--- React-mini-project/src/components/Section1/RightContent.jsx
+++ React-mini-project/src/components/Section1/RightContent.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const RightContent = () => {
-  return <div className="h-full w-2/3 bg-amber-800">RightContent</div>;
+  return <div className="h-full w-3/4 bg-amber-800">RightContent</div>;
 };
 
 export default RightContent;
