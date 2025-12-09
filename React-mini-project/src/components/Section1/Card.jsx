855f045a32bbd8bdb97de04ee3e28ae0316a7ea1
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-12-09 16:54:02 +0530

    modified some files in react mini project

--- React-mini-project/src/components/Section1/Card.jsx
+++ React-mini-project/src/components/Section1/Card.jsx
@@ -8,17 +8,21 @@ const card = () => {
         src="https://plus.unsplash.com/premium_photo-1764545333841-8dd805f5b7c6?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt=""
       />
-      <div className="absolute top-0 left-0 h-full w-full bg-teal-700 p-8 flex flex-col justify-between">
-        <h2>1</h2>
+      <div className="absolute top-0 left-0 h-full w-full text-amber-100 p-6 flex flex-col justify-between">
+        <h2 className="bg-amber-100 rounded-full h-[2vw] w-[2vw] text-black flex justify-center items-center">
+          1
+        </h2>
         <div>
-          <p>
+          <p className="text-[2.5vh]">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut,
             fugiat? Exercitationem voluptates quos ratione impedit.
           </p>
-          <button>Satisfied</button>
-          <button>
-            <i class="ri-arrow-right-up-line text-3xl"></i>
-          </button>
+          <div className="text-[2vh] h-[1.5vw] w-[5vw] bg-amber-100 text-black rounded-full flex justify-center">
+            <button >Satisfied</button>
+            <button>
+              <i class="ri-arrow-right-up-line "></i>
+            </button>
+          </div>
         </div>
       </div>
     </div>
