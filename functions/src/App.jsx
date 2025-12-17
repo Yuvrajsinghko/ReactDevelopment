0252d90d7926132eb832fd7ea102bbaff1f71e4d
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-12-17 15:26:19 +0530

    added new react folder useHook advance

--- functions/src/App.jsx
+++ functions/src/App.jsx
@@ -1,17 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  const clicked=()=>{
-    console.log("Heheuhu");
-    
-    
-  }
+ 
   return (
     <div>
-      <h1>Hello {name}</h1>
-      <button onClick={clicked}>Hii Click me</button>
-    </div>
-  )
-}
+      <div>
 
-export default App
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- useState-Hook/src/App.jsx
+++ useState-Hook/src/App.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [num, setNum] = useState(0);
 
-export default App
+  const updateNum = () => {
+    setNum(num + 1);
+  };
+  const updateNum2 = () => {
+    setNum(num - 1);
+  };
+  const jumpFive=()=>{
+    setNum(num+5)
+  }
+  return (
+    <div style={{ backgroundColor: { num } }} className="ml-[40vw] mt-[10vw]">
+      <div className="flex justify-center flex-col items-center h-[30vw] w-[55vh] bg-gray-600 rounded-4xl">
+        <h1 className="bg-amber-400 text-8xl">{num}</h1>
+        <div className="flex justify-center items-center gap-6 mt-6">
+          <button className="border-2 rounded-4xl text-7xl" onClick={updateNum}>
+            +
+          </button>
+          <button
+            className="border-2  rounded-4xl text-7xl"
+            onClick={updateNum2}
+          >
+            -
+          </button>
+          <button className="border-2 rounded-4xl text-7xl" onClick={jumpFive}>
+            Jump by 5
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
