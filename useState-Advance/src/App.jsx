a4dd5954ec915fc7af714fd8d5f89b371a01de1a
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-12-19 10:44:47 +0530

    added new project folder to understand react form handling

--- useState-Advance/src/App.jsx
+++ useState-Advance/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [num, setnum] = useState([20,30,20]);
+  const btnCliked = () => {
+    
+    
+    
+    
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="mt-58 flex justify-center flex-col items-center gap-10">
+      <h2 className=" text-[12vw]">{num}</h2>
+      <button className="border text-8xl "  onClick={btnCliked}>Increment</button>
+    </div>
+  );
+};
 
-export default App
+export default App;
