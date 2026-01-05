834d13c2d90c25c73f47fee3e10ecaeae883bbe6
Yuvrajsinghko <yuvrajprince19@gmail.com>
2026-01-05 18:19:11 +0530

    added new compnent Result grid

---
+++ Meme-Gen/src/components/ResultGrid.jsx
@@ -0,0 +1,11 @@
+import {fetchPhotos,fetchVideos,fetchGIF} from '../api/mediaApi'
+
+import {setQuery,setLoading,setError,setResults } from '../redux/features/searchSlice'
+
+const ResultGrid = () => {
+  return (
+    <div>ResultGrid</div>
+  )
+}
+
+export default ResultGrid
