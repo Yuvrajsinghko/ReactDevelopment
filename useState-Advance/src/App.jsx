a0db13768081bdfee38203627d52939c507a466f
Yuvrajsinghko <yuvrajprince19@gmail.com>
2026-02-27 19:04:00 +0530

    added some small animation using gsap

--- useState-Advance/src/App.jsx
+++ useState-Advance/src/App.jsx
@@ -1,18 +1,48 @@
-import React, { useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef, useState } from "react";
 
 const App = () => {
-  const [num, setnum] = useState([20,30,20]);
-  const btnCliked = () => {
-    
-    
-    
-    
-  };
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [roti, setRot] = useState(0);
+  const imgRef=useRef();
+  const randomX = gsap.utils.random(-300, 300, 10);
+  const randomY = gsap.utils.random(-300, 300, 10);
+  const rot = gsap.utils.random(-360, 360, 30);
+
+  useGSAP(() => {
+    gsap.to(imgRef.current, {
+      x: x,
+      y:y,
+      duration: 0.7,
+      rotate: roti,
+    });
+  }, {scope:'main',dependencies:[x,y,roti]});
   return (
-    <div className="mt-58 flex justify-center flex-col items-center gap-10">
-      <h2 className=" text-[12vw]">{num}</h2>
-      <button className="border text-8xl "  onClick={btnCliked}>Increment</button>
-    </div>
+    <main className="flex flex-col justify-center items-center min-h-screen w-full bg-zinc-900 gap-20">
+      {/* <div className="b flex flex-col items-center gap-5">
+        <div className="circle h-55 w-55 bg-amber-700 rounded-full "></div>
+        <div className="box h-55 w-55 bg-amber-600 rounded-3xl mb-5"></div>
+      </div>
+
+      <div className="a flex flex-col items-center gap-5">
+        <div className="circle h-55 w-55 bg-cyan-600 rounded-full"></div>
+        <div className="box h-55 w-55 bg-cyan-500 rounded-3xl mb-5"></div>
+      </div> */}
+      
+      <img
+        onClick={() => {
+         setX(randomX)
+         setY(randomY)
+         setRot(rot)
+        }}
+        ref={imgRef}
+        className="dragon h-45 w-45"
+        src="./dragonfly.png"
+        alt="Dragon Fly Image"
+      />
+    </main>
   );
 };
 
