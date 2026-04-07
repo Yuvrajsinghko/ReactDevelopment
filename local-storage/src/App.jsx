5a240764db7232a4ca759caa80b4af7e58d85ae4
Yuvrajsinghko <yuvrajprince19@gmail.com>
2026-04-07 23:37:22 +0530

    updated some files in local-storage react small project

--- local-storage/src/App.jsx
+++ local-storage/src/App.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
 
 const App = () => {
-  const user = {
-    name:'Yuvraj',
-    age:25,
-    gender:'male'
-  }
-  localStorage.setItem('user',JSON.stringify(user))
+  // const user = {
+  //   name:'Yuvraj',
+  //   age:25,
+  //   gender:'male'
+  // }
+  // localStorage.setItem('user',JSON.stringify(user))
   
-  const user1 = JSON.parse(localStorage.getItem("user"));
-  console.log(user1);
-  
+  // const user1 = JSON.parse(localStorage.getItem("user"));
+  // console.log(user1);
 
   return (
-    <div className='text-4xl font-extrabold h-screen bg-cyan-800'>
-      App
+    <div className='text-4xl font-extrabold h-screen bg-[#333]'>
+      
     </div>
   )
 }
